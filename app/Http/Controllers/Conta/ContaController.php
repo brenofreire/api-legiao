11ef445f50634fe1a3c4d78e305028a117852c27
breno freire <breno.loorran@gmail.com>
2019-08-23 00:55:06 -0300

    [Perfil] correção elo case when

--- app/Http/Controllers/Conta/ContaController.php
+++ app/Http/Controllers/Conta/ContaController.php
@@ -80,7 +80,6 @@ class ContaController extends Controller
             ])->with(['pontuacao' => function ($query) {
                 $query->select(
                     DB::raw('                    
-                    (
                         CASE 
                             WHEN SUM(pontuacao) BETWEEN 0 AND 50 THEN "Cobre" 
                             WHEN SUM(pontuacao) BETWEEN 51 AND 100 THEN "Bronze" 
@@ -88,8 +87,7 @@ class ContaController extends Controller
                             WHEN SUM(pontuacao) BETWEEN 251 AND 500 THEN "Ouro" 
                             WHEN SUM(pontuacao) BETWEEN 501 AND 800 THEN "Diamante" 
                             WHEN SUM(pontuacao) > 801 THEN "Platina" 
-                        END 
-                    ) AS elo,
+                        END AS elo,
                     SUM(pontuacao) as pontuacao_soma,cid
                 '))->where([
                         ['status', '=', 1]
